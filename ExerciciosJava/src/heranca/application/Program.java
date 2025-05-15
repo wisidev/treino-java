4417a13ad0a9f38b0d0d9c12a1160db40a018c3e
Willian <willianguis@outlook.com>
2025-05-14 21:57:32 -0300

    exerc. herança

--- ExerciciosJava/src/heranca/application/Program.java
+++ ExerciciosJava/src/heranca/application/Program.java
@@ -8,6 +8,20 @@ public class Program {
 
 	public static void main(String[] args) {
 		
+		// AULA 2
+		Account acc1 = new Account(1001, "Alex", 1000.0);
+		acc1.withdraw(220.0);
+		System.out.println(acc1.getBalance());
+		
+		Account acc2 = new SavingsAccount(1002, "Maria", 1000.0, 0.01);
+		acc2.withdraw(200.0);
+		System.out.println(acc2.getBalance());
+		
+		Account acc3 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
+		
+		/* AULA 1
 		Account acc = new Account(1001, "Alex", 0.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 		
@@ -33,5 +47,6 @@ public class Program {
 			acc5.updateBalance();
 			System.out.println("Update!");
 		}
+		*/
 	}
 }
